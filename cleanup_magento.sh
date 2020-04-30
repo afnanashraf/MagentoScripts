768ecdc9208f91cf7cc5876c6d32d08f0b4f47a1
Afnan Ashraf <afnan.kuberiter@gmail.com>
2020-04-30 11:56:29 +0530

    Magento cleanup script added

---
+++ cleanup_magento.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+USER="$1"
+
+MAGENTO2_PATH=/home/$USER/MYMAGENTO2/
+
+cd $MAGENTO2_PATH && ./kill
+
+if [ $? -eq 0 ]; then
+    sudo rm -rf $MAGENTO2_PATH
+    echo SUCCESS
+else
+    echo FAIL
+fi
+
+
+
+
+
+
